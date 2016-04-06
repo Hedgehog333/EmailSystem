363b76715f4c944f7d8aef70e2155acc873add02
Hedgehog333 <artem1100.94@gmail.com>
2016-04-07 02:20:30 +0300

    edit

--- application/views/authentication_view.php
+++ application/views/authentication_view.php
@@ -14,9 +14,9 @@
 
 <h1>Аутентификация</h1>
 <form action="authentication" method="post">
-    <label>Password*</label>
+    <label>Password</label>
     <input class="input" type="password" name="password" size="50" /><br />
-    <label>Repeat password*</label>
+    <label>Repeat password</label>
     <input class="input" type="password" name="repeatPassword" size="50" /><br />
     <input type="hidden" name="token" value="<?php echo $_GET["token"] ?>" />
     <input type="submit" value="Регистрация"/>
