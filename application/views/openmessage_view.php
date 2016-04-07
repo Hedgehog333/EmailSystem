f34575749e75d5c07cb273c13d8cceadd5bba6af
Hedgehog333 <artem1100.94@gmail.com>
2016-04-07 05:30:56 +0300

    default layout

--- application/views/openmessage_view.php
+++ application/views/openmessage_view.php
@@ -1,2 +1,24 @@
-    <h1>Message</h1>
-    
+<style type="text/css">
+    ul li {
+        float: left;
+        margin: 2px 10px;
+    }
+    ul {
+        overflow: hidden;
+    }
+    .panel-primary > *{
+        padding: 8px 20px;
+    }
+</style>
+
+<span class="date">10.10.2081</span>
+<h3>Title</h3>
+<ul>
+    <li>from</li>
+    <li>from</li>
+    <li>from</li>
+</ul>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
